c5bf37bca8c39329137293184dfd0eeb44f4d00b
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-05-06 22:27:25 -0300

    1002 - Área do Círculo

---
+++ SUBMISSÕES/1002.kt
@@ -0,0 +1,14 @@
+// 1002 - Área do Círculo
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    
+    val reader = Scanner(System.`in`);
+	val a = 3.14159;
+	val b = reader.nextDouble()
+	val c = a * b * b;
+
+        println("A=%.4f".format(c))
+	
+}
