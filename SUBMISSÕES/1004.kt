d91e1ba4ec5082025790f35b280bbe63b4630053
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-05-06 22:27:51 -0300

    1004 - Produto Simples

---
+++ SUBMISSÕES/1004.kt
@@ -0,0 +1,12 @@
+// 1004 - Produto Simples
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+	val reader = Scanner(System.`in`)
+    val x = reader.nextInt()
+    val y = reader.nextInt()
+    println("PROD = "+ (x * y))
+	
+}
