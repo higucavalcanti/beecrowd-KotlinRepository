1ec04e59e62e8348c6c16e3acebf173c15851481
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-05-06 22:27:40 -0300

    1003 - Soma Simples

---
+++ SUBMISSÕES/1003.kt
@@ -0,0 +1,14 @@
+// 1003 - Soma Simples
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+	val reader = Scanner(System.`in`)
+	val A = reader.nextInt()
+	val B = reader.nextInt()
+	val C = A + B
+	
+	println("SOMA = "+C)
+	
+}
