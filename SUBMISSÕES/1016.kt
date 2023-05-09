045822401e641d1908826eae4ed69c682c5aa42d
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-05-08 23:16:05 -0300

    1016 - Distância

---
+++ SUBMISSÕES/1016.kt
@@ -0,0 +1,12 @@
+// 1016 - Distância
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+val reader = Scanner(System.`in`)
+
+val km = reader.nextInt()
+val min = (60 * km) / 30
+println("$min minutos")
+}
